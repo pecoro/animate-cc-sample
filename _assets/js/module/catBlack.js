33d55e878d51935c6b2dbadeeabebd6921aa0141
Yukiko Tatsukawa <t.yukiko0321@gmail.com>
2019-12-10 15:51:06 +0100

    catBlackとcatBlueのクラスの土台を用意する

---
+++ _assets/js/module/catBlack.js
@@ -0,0 +1,4 @@
+export default class CatBlack {
+    constructor(opts = {}) {
+    }
+};
--- _assets/js/project.js
+++ _assets/js/project.js
@@ -1,1 +1,7 @@
 "use strict"
+
+import CatBlack from './module/catBlack.js';
+import CatBlue from './module/catBlue.js';
+
+let catBlack = new CatBlack();
+let catBlue = new CatBlue();
